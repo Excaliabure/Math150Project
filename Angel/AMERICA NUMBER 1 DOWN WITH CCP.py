d93642b22b101ca9b4481a85d371d011bcba8554
Excaliabure <106698782+Excaliabure@users.noreply.github.com>
2024-04-01 16:42:43 -0700

    Start on project
    AMERICA # 1 RUSSIA 0 AMERICA BIG OIIL AMERICA WIN

---
+++ Angel/AMERICA NUMBER 1 DOWN WITH CCP.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+import datetime as dt
+import math as m
+import sklearn.linear_model as sk
+import sklearn.tree as skt
+
+
+with open('/Users/angelgonzalezguevara/Downloads/pswrgvwall.csv','r') as file:
+    reader = csv.reader(file)
+    next(reader)
+    next(reader)
+    print(next(reader))
+    
+    x=[]
+    x2=[]
+    xdat =[]
+    xdat2=[]
+    for row in reader:
+        x.append(row[0])
+        x2.append(row[1])
+    x.pop()
+    x2.pop()
+
+dates = [1994 + date/len(x) * (2023-1994) for date in range(len(x))]
+x2int = [float(i) for i in x2]
+
+dates = np.array(dates).reshape(-1,1)
+x2int = np.array(x2int)
+
+
+ypred = sk.LinearRegression().fit(dates,x2int).predict(dates)
+rsqu = sk.LinearRegression().fit(dates,x2int).score(dates,x2int)
+
+plt.scatter(dates, x2int,s=[5])
+plt.plot(dates,ypred,color='red')
+plt.title('Pruce of Gas (1994'+ '-Present), R^='+str(np.round(rsqu,2)))
+plt.xlabel('Date')
+
+# plt.grid(True)
+
+# Format date on x-axis
+
+# Rotate and align the x labels
+
+
+# Show plot
+plt.show()
---
+++ README.md
@@ -0,0 +1,10 @@
+Math 150 project
+
+LONG LIVE AMERICA OIL #1 CHINA BAD DOWN WITH THE CCP 
+
+
+Dependancies
+
+pip install numpy
+pip install pandas
+pip install scikit-sklearn
