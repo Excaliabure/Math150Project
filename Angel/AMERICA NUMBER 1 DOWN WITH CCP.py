be7bae3e5f6775bc31dd63ff27aa67da0434b9d7
Angel Gonzalez Guevara <angelgonzalezguevara@Angels-MacBook-Pro-2.local>
2024-04-08 15:11:29 -0700

    please push

--- Angel/AMERICA NUMBER 1 DOWN WITH CCP.py
+++ Angel/AMERICA NUMBER 1 DOWN WITH CCP.py
@@ -4,14 +4,14 @@ import csv
 import datetime as dt
 import math as m
 import sklearn.linear_model as sk
-import sklearn.tree as skt
+import sklearn.cluster as skc
 
-# nice car
+
 with open('/Users/angelgonzalezguevara/Downloads/pswrgvwall.csv','r') as file:
     reader = csv.reader(file)
     next(reader)
     next(reader)
-    print(next(reader))
+    next(reader)
     
     x=[]
     x2=[]
@@ -23,27 +23,69 @@ with open('/Users/angelgonzalezguevara/Downloads/pswrgvwall.csv','r') as file:
     x.pop()
     x2.pop()
 
-dates = [1994 + date/len(x) * (2023-1994) for date in range(len(x))]
-x2int = [float(i) for i in x2]
+with open('/Users/angelgonzalezguevara/Documents/Crude Oil/Data 1-Table 1.csv','r') as file:
+    reader = csv.reader(file)
+    print(next(reader))
+    print(next(reader))
+    print(next(reader))
+    xdates = []
+    xBarPr = []
+    for row in reader:
+        xdates.append(row[0])
+        xBarPr.append(row[1])
+
+dates = [dt.datetime.strptime(date, '%b %d, %Y') for date in x]
+datesforCr = [dt.datetime.strptime(date, '%b %d, %Y') for date in xdates]
+
+datesforR2 = [dt.datetime.strptime(date, '%b %d, %Y').timestamp() for date in x]
+x2int = [float(i) for i in x2]  # gas prices
+x2Cr = [float(i) for i in xBarPr] # crude oil barrel prices
 
 dates = np.array(dates).reshape(-1,1)
-x2int = np.array(x2int)
+x2int = np.array(x2int).reshape(-1,1)
+x2Cr = np.array(x2Cr).reshape(-1,1)
+datesforR2 = np.array(datesforR2).reshape(-1,1)
+
+ypred = sk.LinearRegression().fit(datesforR2,x2int).predict(datesforR2)
+rsqu = sk.LinearRegression().fit(datesforR2,x2int).score(datesforR2,x2int)
+
+xgasp =[]
+xcrudep = []
+for i in range(len(dates)):
+    if dates[i] == datesforCr[i]:
+        continue
+    xgasp.append(x2int[i])
+    xcrudep.append(x2Cr[i])
 
 
-ypred = sk.LinearRegression().fit(dates,x2int).predict(dates)
-rsqu = sk.LinearRegression().fit(dates,x2int).score(dates,x2int)
-
+plt.subplot(1,4,1)
 plt.scatter(dates, x2int,s=[5])
 plt.plot(dates,ypred,color='red')
-plt.title('Pruce of Gas (1994'+ '-Present), R^='+str(np.round(rsqu,2)))
+plt.title('Price of Gas (1994'+ '-Present), R^='+str(np.round(rsqu,2)))
+plt.subplot(1,4,2)
+plt.scatter(datesforCr, x2Cr,s=[5])
 plt.xlabel('Date')
+plt.subplot(1,4,3)
+plt.scatter(xcrudep, xgasp,s=[5])
+plt.grid(True)
 
-# plt.grid(True)
+# cluster
+X = []
+xgaspr = x2int.tolist()
+xcrudepr = x2Cr.tolist()
 
-# Format date on x-axis
+xgaspr = [item for sublist in xgaspr for item in sublist]
+xcrudepr = [item for sublist in xcrudepr for item in sublist]
+for i,j in zip(xcrudepr,xgaspr):
+    X.append([i,j])
 
-# Rotate and align the x labels
 
+kmeans = skc.KMeans(n_clusters=5).fit(X)
+ykmean = skc.KMeans(n_clusters=5).fit(X).predict(X)
+crudeoilprices = [price[0] for price in X]
+gasprices = [price[1] for price in X]
 
 # Show plot
+plt.subplot(1,4,4)
+plt.scatter(crudeoilprices,gasprices, c = ykmean)
 plt.show()
